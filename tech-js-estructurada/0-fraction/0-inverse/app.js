beb45cdb93eee9d27a184fd7dd99af33eccd42e9
“DIEGO <diegonovoa1981@hotmail.com>
2022-05-04 05:44:57 -0500

    cree nueva carpeta

---
+++ tech-js-estructurada/0-fraction/0-inverse/app.js
@@ -0,0 +1,8 @@
+const { Console } = require("console-mpds");
+const console = new Console();
+
+const numerador   = console.readNumber("Introduce el numerador de la fracción: ");;
+const denominador = console.readNumber("Introduce el denominador de la fracción: ");;
+
+
+console.writeln(`La fracción ${numerador}/${denominador} invertida es la fracción ${denominador}/${numerador}`);
