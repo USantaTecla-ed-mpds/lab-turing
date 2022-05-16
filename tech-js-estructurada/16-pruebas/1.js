5012df8036c98d6900af046957ed1a5acb51cc76
Joselon <jose.navarro.osta@gmail.com>
2022-05-16 21:00:08 +0100

    Formateado

--- tech-js-estructurada/16-pruebas/1.js
+++ tech-js-estructurada/16-pruebas/1.js
@@ -6,42 +6,42 @@ const { Console } = require("console-mpds");
 const console = new Console();
 
 let phrase = console.readString("Escribe una frase para determinar si es o no un PALÍNDROMO (sin mayúsculas):");
-let compactPhraseArray=[];
-let j=0;
-for (let i=0;i<phrase.length;i++){
-    if (phrase[i]!=" "){
-        compactPhraseArray[j]=phrase[i];
+let compactPhraseArray = [];
+let j = 0;
+for (let i = 0; i < phrase.length; i++) {
+    if (phrase[i] != " ") {
+        compactPhraseArray[j] = phrase[i];
         j++;
     }
 }
-let reverseCompactPhraseArray=[];
-for (let i=0;i<compactPhraseArray.length;i++){
-    switch(compactPhraseArray[i]){
+let reverseCompactPhraseArray = [];
+for (let i = 0; i < compactPhraseArray.length; i++) {
+    switch (compactPhraseArray[i]) {
         case "á":
-            compactPhraseArray[i]="a";
+            compactPhraseArray[i] = "a";
             break;
         case "é":
-            compactPhraseArray[i]="e";
+            compactPhraseArray[i] = "e";
             break;
         case "í":
-            compactPhraseArray[i]="e";
-            [i]="i";
+            compactPhraseArray[i] = "e";
+            [i] = "i";
             break;
         case "ó":
-            compactPhraseArray[i]="o";
+            compactPhraseArray[i] = "o";
             break;
         case "ú":
-            compactPhraseArray[i]="u";
+            compactPhraseArray[i] = "u";
             break;
         default:
     }
-    reverseCompactPhraseArray[(compactPhraseArray.length-1)-i]=compactPhraseArray[i];
+    reverseCompactPhraseArray[(compactPhraseArray.length - 1) - i] = compactPhraseArray[i];
 }
-let isPalindrome=true;
-for (let letterIndex in compactPhraseArray){
-    isPalindrome &= compactPhraseArray[letterIndex]===reverseCompactPhraseArray[letterIndex];
+let isPalindrome = true;
+for (let letterIndex in compactPhraseArray) {
+    isPalindrome &= compactPhraseArray[letterIndex] === reverseCompactPhraseArray[letterIndex];
 }
-console.writeln(`La frase "${phrase}" ${isPalindrome?"sí":"no"} es un palíndromo`);
+console.writeln(`La frase "${phrase}" ${isPalindrome ? "sí" : "no"} es un palíndromo`);
 
 
 
