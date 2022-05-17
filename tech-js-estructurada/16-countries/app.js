837195bf46bd85089e21db294ad001d6c14c2586
Joselon <jose.navarro.osta@gmail.com>
2022-05-17 21:12:58 +0100

    EjericicioCountriesOK

--- tech-js-estructurada/16-countries/app.js
+++ tech-js-estructurada/16-countries/app.js
@@ -9,7 +9,7 @@ Uruguay, Paraguay, Gambia, Djibouti, Martinique, Guam, Georgia, United States, B
 2. Buscar país
 3. Salir
 Escoge opción? [1-3]:  2
-Dame el nombre del país:  Spain
+  Spain
 El país "Spain" si está incluido
 
 1. Ver países
@@ -37,14 +37,26 @@ const { Console } = require("console-mpds");
 const console = new Console();
 
 let optionSelected;
+do {
 
-/*do{
+    optionSelected = console.readNumber(` 1. Ver países \n\ 2. Buscar país\n\ 3. Salir \n\ Escoge opción? [1-3]:`);
+    switch (optionSelected) {
+        case 1:
+            console.writeln(`${NAMES}`);
+            break;
+        case 2:
+            const searchedCountry = console.readString("Dame el nombre del país(en inglés):");
+            let found = false;
+            for (let i in NAMES) {
+                if (NAMES[i] === searchedCountry) {
+                    found = true;
+                }
+            }
+            console.writeln(`El país "${searchedCountry}" ${found ? 'sí' : 'no'} está incluido`)
+            break;
+        default:
+    }
 
-    optionSelected=console.readNumber(`1. Ver países /n 
-    2. Buscar país
-    3. Salir`)
 
-
-
-}while(optionSelected===3);*/
+} while (optionSelected != 3);
 
