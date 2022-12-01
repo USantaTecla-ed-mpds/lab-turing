81d73e54368cf6adda6b60e21107112b7ebec66d
Joselon <joselon79@gmail.com>
2022-12-01 23:51:29 +0000

    comentarios

--- tech-dhtml/connect4/main.js
+++ tech-dhtml/connect4/main.js
@@ -571,6 +571,16 @@ class HumanPlayerView extends PlayerView {
           reject(error);
         }
       });
+getUsers() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        { username: 'john', email: 'john@test.com' },
+        { username: 'jane', email: 'jane@test.com' },
+      ]);
+    }, 1000);
+  });
+}
 */
 
 }
