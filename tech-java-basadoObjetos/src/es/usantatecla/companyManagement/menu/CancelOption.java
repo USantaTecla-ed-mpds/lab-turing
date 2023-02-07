e7ba10774a59d8b1e5e08dcad5c78014f55fb19d
xavi gomez <jumpering@gmail.com>
2023-02-07 22:42:05 +0100

    CancelOption implementation

--- tech-java-basadoObjetos/src/es/usantatecla/companyManagement/menu/CancelOption.java
+++ tech-java-basadoObjetos/src/es/usantatecla/companyManagement/menu/CancelOption.java
@@ -1,5 +1,8 @@
 package es.usantatecla.companyManagement.menu;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import es.usantatecla.companyManagement.ServicesContract;
 import es.usantatecla.utils.Console;
 import es.usantatecla.utils.Date;
@@ -13,12 +16,20 @@ public class CancelOption implements Option{
 
     @Override
     public void execute(ServicesContract servicesContract) {
-        String userInputDate = new Console().readString("Día y mes [dd/mm]: ");
-        //^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])
-        //dia
-        //mes
-        //servidacc-getYear
-        Date date = new Date();
+        Pattern pattern = Pattern.compile("^([0-2][0-9]|3[0-1])(/)(0[1-9]|1[0-2])");
+        Matcher matcher;
+        String userInputDate;
+        do{
+            userInputDate = new Console().readString("Día y mes [dd/mm]: ");
+            matcher = pattern.matcher(userInputDate);
+            if(!matcher.matches()){
+                System.out.println("ERROR! Día y mes con el formato [dd/mm]");
+            }
+        }while(!matcher.matches());
+        String[] separatedInput = userInputDate.split("/");
+        int day = Integer.parseInt(separatedInput[0]);
+        int month = Integer.parseInt(separatedInput[1]);
+        Date date = new Date(day, month, servicesContract.getYear());
         servicesContract.cancel(date);
     }
     
