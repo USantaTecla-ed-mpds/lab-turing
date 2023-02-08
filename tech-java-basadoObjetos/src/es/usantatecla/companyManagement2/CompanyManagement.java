9286e5a715151d4265f39f24e9714ad7c70f3b0a
Joselon <joselon79@gmail.com>
2023-02-08 21:20:29 +0000

    PendienteVersion2

--- tech-java-basadoObjetos/src/es/usantatecla/companyManagement2/CompanyManagement.java
+++ tech-java-basadoObjetos/src/es/usantatecla/companyManagement2/CompanyManagement.java
@@ -1,15 +1,16 @@
 package es.usantatecla.companyManagement2;
+import es.usantatecla.companyManagement2.utils.Console;
 
 public class CompanyManagement {
 
 
     public static void main(String[] args) {
-        
+        new CompanyManagement().run();
     }
 
     public void run(){
 
-        
+        new Console().writeln("Gestión del contrato");
     }
     
 }
