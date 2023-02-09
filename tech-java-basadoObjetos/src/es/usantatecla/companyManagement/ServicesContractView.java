052ca24b5f8185ebed67786770b4eb9b23e751d8
xavi gomez <jumpering@gmail.com>
2023-02-09 21:56:31 +0100

    refactor UML and remove DateView.java

--- tech-java-basadoObjetos/src/es/usantatecla/companyManagement/ServicesContractView.java
+++ tech-java-basadoObjetos/src/es/usantatecla/companyManagement/ServicesContractView.java
@@ -1,5 +1,8 @@
 package es.usantatecla.companyManagement;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import es.usantatecla.utils.Console;
 import es.usantatecla.utils.Date;
 
@@ -12,12 +15,12 @@ public class ServicesContractView {
     }
 
     public void cancel() {
-        Date date = new DateView().getFromDayAndMonthUserInput(servicesContract.getYear());
+        Date date = this.getFromDayAndMonthUserInput(servicesContract.getYear());
         this.servicesContract.cancel(date);
     }
 
     public void enlarge() {
-        Date date = new DateView().getFromDayAndMonthUserInput(servicesContract.getYear());
+        Date date = this.getFromDayAndMonthUserInput(servicesContract.getYear());
         double scale;
         boolean validScale = false;
         Console console = new Console();
@@ -33,7 +36,7 @@ public class ServicesContractView {
     }
 
     public void shift() {
-        Date date = new DateView().getFromDayAndMonthUserInput(servicesContract.getYear());
+        Date date = this.getFromDayAndMonthUserInput(servicesContract.getYear());
         double shiftment;
         boolean validShifment = false;
         Console console = new Console();
@@ -68,4 +71,20 @@ public class ServicesContractView {
         return this.servicesContract.getYear();
     }
 
+    private Date getFromDayAndMonthUserInput(int year){ //ojo acoplamiento console() todo ..igual no va aqui este método
+        Pattern pattern = Pattern.compile("^([0-2][0-9]|3[0-1])(/)(0[1-9]|1[0-2])");
+        Matcher matcher;
+        String userInputDate;
+        do{
+            userInputDate = new Console().readString("Día y mes [dd/mm]: ");
+            matcher = pattern.matcher(userInputDate);
+            if(!matcher.matches()){
+                System.out.println("ERROR! Día y mes con el formato [dd/mm]");
+            }
+        }while(!matcher.matches());
+        String[] separatedInput = userInputDate.split("/");
+        int day = Integer.parseInt(separatedInput[0]);
+        int month = Integer.parseInt(separatedInput[1]);
+        return new Date(day, month, year);
+    }
 }
