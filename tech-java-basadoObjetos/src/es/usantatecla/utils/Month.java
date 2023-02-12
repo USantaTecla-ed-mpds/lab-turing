a3421ff1b3c2a48e520391d265e5ba4295246466
xavi gomez <jumpering@gmail.com>
2023-02-12 20:24:46 +0100

    add month enum

---
+++ tech-java-basadoObjetos/src/es/usantatecla/utils/Month.java
@@ -0,0 +1,23 @@
+package es.usantatecla.utils;
+
+public enum Month {
+    
+    JANUARY(31, new String[]{ "Enero", "January" }),
+    FEBRUARY(28, new String[]{ "Febrero", "Februry" });
+
+    int days;
+    String[] langs;
+
+    private Month(int days, String[] langs){
+        this.days = days;
+        this.langs = langs;
+    }
+
+    public static Month getMonth(int numberOfMonth){
+        return Month.values()[numberOfMonth -1];
+    }
+
+    public int getNumberOfDays(){
+        return this.days;
+    }
+}
