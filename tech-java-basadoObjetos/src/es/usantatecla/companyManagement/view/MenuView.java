d0a87a76d25ab84a7d96875f3f395d68902fefd1
xavi gomez <jumpering@gmail.com>
2023-02-19 08:40:26 +0100

    reduce menuview show metohd size

--- tech-java-basadoObjetos/src/es/usantatecla/companyManagement/view/MenuView.java
+++ tech-java-basadoObjetos/src/es/usantatecla/companyManagement/view/MenuView.java
@@ -16,43 +16,63 @@ public class MenuView {
     }
 
     public void show(Languaje languaje) {
-        Console console = new Console();
-        int selectedOption = 0;
-        boolean validOption = false;
-        console.writeln(
-                Message.MENU_TITLE_PRE.getCustomLanguajeMessage(languaje)
-                        +
-                        this.servicesContractView.getName()
-                        +
-                        Message.MENU_TITLE_POST.getCustomLanguajeMessage(languaje)
-                        +
-                        this.servicesContractView.getYear());
+        int selectedOption;
+        this.showMenuTitle(languaje);
         do {
-            console.writeln(Message.MENU_SUBTITLE.getCustomLanguajeMessage(languaje));
+            this.showSubMenuTitle(languaje);
             for (int i = 0; i < this.menu.getSize(); i++) {
-                console.writeln(i + 1
-                        +
-                        Message.MENU_SEPARATOR.getCustomLanguajeMessage(languaje)
-                        +
-                        this.menu.getOption(i).showTitle(languaje));
+                this.showOptionTitle(i, languaje);
             }
-            selectedOption = console.readInt(
-                    Message.MENU_READ_ERROR_PRE.getCustomLanguajeMessage(languaje)
-                            +
-                            this.menu.getSize()
-                            +
-                            Message.MENU_READ_ERROR_POST.getCustomLanguajeMessage(languaje));
-            validOption = selectedOption > 0 && selectedOption <= this.menu.getSize();
+            selectedOption = this.readUserOption(languaje);
+            boolean validOption = selectedOption > 0 && selectedOption <= this.menu.getSize();
             if (validOption) {
                 this.menu.getOption(selectedOption - 1).execute(this.servicesContractView);
             } else {
-                console.writeln(
-                        Message.MENU_SELECT_ERROR_PRE.getCustomLanguajeMessage(languaje)
-                                +
-                                this.menu.getSize()
-                                +
-                                Message.MENU_SELECT_ERROR_POST.getCustomLanguajeMessage(languaje));
+                this.showErrorSelectedOption(languaje);
             }
         } while (selectedOption != this.menu.getSize());
+    }
+
+    private void showMenuTitle(Languaje languaje) {
+        Console console = new Console();
+        console.writeln(
+            Message.MENU_TITLE_PRE.getCustomLanguajeMessage(languaje)
+                +
+                this.servicesContractView.getName()
+                +
+                Message.MENU_TITLE_POST.getCustomLanguajeMessage(languaje)
+                +
+                this.servicesContractView.getYear());
+    }
+
+    private void showSubMenuTitle(Languaje languaje) {
+        new Console().writeln(Message.MENU_SUBTITLE.getCustomLanguajeMessage(languaje));
+    }
+
+    private void showOptionTitle(int i, Languaje languaje) {
+        new Console().writeln(
+            i + 1
+                +
+                Message.MENU_SEPARATOR.getCustomLanguajeMessage(languaje)
+                +
+                this.menu.getOption(i).showTitle(languaje));
+    }
+
+    private int readUserOption(Languaje languaje){
+        return new Console().readInt(
+            Message.MENU_READ_ERROR_PRE.getCustomLanguajeMessage(languaje)
+                    +
+                    this.menu.getSize()
+                    +
+                    Message.MENU_READ_ERROR_POST.getCustomLanguajeMessage(languaje));
+    }
+
+    private void showErrorSelectedOption(Languaje languaje){
+        new Console().writeln(
+            Message.MENU_SELECT_ERROR_PRE.getCustomLanguajeMessage(languaje)
+                    +
+                    this.menu.getSize()
+                    +
+                    Message.MENU_SELECT_ERROR_POST.getCustomLanguajeMessage(languaje));
     }
 }
